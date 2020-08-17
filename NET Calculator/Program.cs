6ff306402336789a0e24a3560d5bc9352e2d4dea
Marius Constantin Ciobanu <ciobanumariusro@gmail.com>
2020-08-17 15:51:19 +0300

    Created and renamed the first form. Added 2 buttons which were renamed to ButtonCalculatorForm and ButtonAboutForm. Looked at the size of the 1st button and modified the size to the 2nd one so that it is more visually pleasing.

--- NET Calculator/Program.cs
+++ NET Calculator/Program.cs
@@ -16,7 +16,7 @@ namespace NET_Calculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormHomepage());
         }
     }
 }
